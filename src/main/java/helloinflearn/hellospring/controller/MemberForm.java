026fe54628deb7aab26b602254dd192d4938ac8d
Junaem <junh9547@gmail.com>
2022-01-11 00:21:21 +0900

    210110 ljh

---
+++ src/main/java/helloinflearn/hellospring/controller/MemberForm.java
@@ -0,0 +1,13 @@
+package helloinflearn.hellospring.controller;
+
+public class MemberForm {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
