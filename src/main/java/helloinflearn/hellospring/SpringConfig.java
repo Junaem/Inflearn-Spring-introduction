11265c5de9e5ff90c525104a7ae475597e129eef
Junaem <junh9547@gmail.com>
2022-01-14 02:25:08 +0900

    220114 JPA

--- src/main/java/helloinflearn/hellospring/SpringConfig.java
+++ src/main/java/helloinflearn/hellospring/SpringConfig.java
@@ -1,34 +1,37 @@
 package helloinflearn.hellospring;
 
-import helloinflearn.hellospring.repository.JdbcMemberRepository;
-import helloinflearn.hellospring.repository.MemberRepository;
-import helloinflearn.hellospring.repository.MemoryMemberRepository;
+import helloinflearn.hellospring.repository.*;
 import helloinflearn.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+//    private DataSource dataSource;
+//    private EntityManager em;
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
+
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
-    }
+//        return new JdbcMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+
+//    }
 
 }
---
+++ src/main/java/helloinflearn/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,12 @@
+package helloinflearn.hellospring.repository;
+
+import helloinflearn.hellospring.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    @Override
+    Optional<Member> findByName(String name);
+}
